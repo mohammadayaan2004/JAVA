1e6ebacf660a9253546180f6f49c67ab4c825a43
Mohammad Ayaan Khan <mohammad.ayaan2004@gmail.com>
2025-01-04 23:44:34 +0530

    Your commit message

--- self study/prog68_HallowTraianglePattern.java
+++ self study/prog68_HallowTraianglePattern.java
@@ -1,15 +1,21 @@
-
+/*
+     *
+    * *
+   *   *
+  * * * *
+*/
 import java.util.Scanner;
 public class prog68_HallowTraianglePattern {
-    public static void nForest(int n)
-    {
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=n-i;j++)
-            {
-                System.out.print(" ");
+    public static void nForest(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= 2 * n - 1; j++) {
+                if (i + j == n + 1 || j - i == n - 1 || (i == n && j % 2 == 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
             }
-            System.out.println(" ");
+            System.out.println();
         }
     }
     public static void main(String[] args) {
