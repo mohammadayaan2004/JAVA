28da4c76d88f962eabcbc724397dcd6f2d84df9b
Mohammad Ayaan Khan <mohammad.ayaan2004@gmail.com>
2025-01-05 23:38:44 +0530

    Your commit message

--- self study/prog69_HallowReverseTrianglePattern.java
+++ self study/prog69_HallowReverseTrianglePattern.java
@@ -1,16 +1,17 @@
 /*
-     *
-    * *
-   *   *
-  * * * *
-*/
+* * * * * 
+ *     *
+  *   *   
+   * *   
+    *     
+ */
 import java.util.Scanner;
-public class prog68_HallowTraianglePattern {
+public class prog69_HallowReverseTrianglePattern {
     public static void nForest(int n) {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= 2 * n - 1; j++) 
             {
-                if (i + j == n + 1 || j - i == n - 1 || (i == n && j % 2 == 1)) 
+                if (i==j || (i==1 && j%2==1) || i+j == 2*n ) 
                 {
                     System.out.print("*");
                 } 
