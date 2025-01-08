525f0e6517a581a5b0f585e7114c8c8ee0aafbda
Mohammad Ayaan Khan <mohammad.ayaan2004@gmail.com>
2025-01-08 13:55:43 +0530

    Your commit message

--- self study/prog70_PascalTraingle.java
+++ self study/prog70_PascalTraingle.java
@@ -7,26 +7,25 @@
 import java.util.Scanner;
 public class prog70_PascalTraingle  {
     public static void nForest(int n) {
-        int k=1;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <=n-i ; j++) 
             {
                System.out.print(" ");
             }
-            k=1;
+            int k=1;
             for(int j=1;j<=i;j++)
             {
                 System.out.print(k+" ");
-                k=k*(i-j)/(j);  //nCr
+                k=k*(i-j)/(j);  
             }
             
             System.out.println(" ");
         }
-    }
+    }    
     public static void main(String[] args) {
         System.out.print("Number of rows :- ");
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int n= sc.nextInt();
         nForest(n);
         sc.close();
     }
