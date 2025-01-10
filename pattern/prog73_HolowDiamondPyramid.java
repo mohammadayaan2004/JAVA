c2eedc4013bf47b69c71085cf92ed0a15906cfda
Mohammad Ayaan Khan <mohammad.ayaan2004@gmail.com>
2025-01-10 19:54:18 +0530

    Your commit message

--- pattern/prog73_HolowDiamondPyramid.java
+++ pattern/prog73_HolowDiamondPyramid.java
@@ -1,44 +1,42 @@
 /*
- * * * *
-  *   *   
-   * *     
-    *
-   * *
-  *   *
- * * * *  
+   *    
+  * *   
+ *   *  
+*     * 
+ *   *  
+  * *   
+   * 
  */
-
 import java.util.Scanner;
-public class prog72_HollowHourGlassPyramid {
+public class prog73_HolowDiamondPyramid  {
     public static void nForest(int n) {
-        //UPPER HALF
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= 2 * n - 1; j++) {
-                if ((i == 1 && j % 2 == 1) || i == j || i + j == 2 * n) {
+            for (int j = 1; j <=2*n-1 ; j++)
+            {
+                if ((i + j == n+1)||j-i == n-1) {
                     System.out.print("*");// PRINT
                 } else {
                     System.out.print(" ");// SPACE
                 }
-            }
+            } 
             System.out.println(" ");
         }
-        //LOWER HALF
         for (int i = 2; i <= n; i++) {
-            for (int j = 1; j <= 2 * n - 1; j++) 
+            for (int j = 1; j <=2*n-1 ; j++)
             {
-                if ((i == n && j % 2 == 1) ||i + j == n + 1 || j - i == n - 1 ) {
-                    System.out.print("*");//PRINT
+                if ((i + j == 2*n)||j-i == 0) {
+                    System.out.print("*");// PRINT
                 } else {
-                    System.out.print(" ");//SPACE
+                    System.out.print(" ");// SPACE
                 }
-            }
+            } 
             System.out.println(" ");
         }
-    }
+    }    
     public static void main(String[] args) {
         System.out.print("Number of rows :- ");
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int n= sc.nextInt();
         nForest(n);
         sc.close();
     }
