4df7eb63132ad27b906826431dadcbf4c371fe5d
Mohammad Ayaan Khan <mohammad.ayaan2004@gmail.com>
2025-01-10 22:11:43 +0530

    Your commit message

--- pattern/prog74_RightPascalTriangle.java
+++ pattern/prog74_RightPascalTriangle.java
@@ -1,19 +1,19 @@
 /*
-   *    
-  * *   
- *   *  
-*     * 
- *   *  
-  * *   
-   * 
+*    
+ *   
+* *  
+ * * 
+* *  
+ *   
+* 
  */
 import java.util.Scanner;
 public class prog74_RightPascalTriangle {
     public static void nForest(int n) {
-        for (int i = 1; i <=2*n-1; i++) {
-            for (int j = 1; j <=n ; j++)
+        for (int i = 1; i <=n; i++) {
+            for (int j = 1; j <=i; j++)
             {
-                if ((i==j)||(i+j==2*n)||(i%2==0 && j%2==0)||(i%2==1 && j%2==1)) {
+                if ((i%2==0 && j%2==0)||(i%2==1 && j%2==1)) {
                     System.out.print("*");// PRINT
                 } else {
                     System.out.print(" ");// SPACE
@@ -21,6 +21,18 @@ public class prog74_RightPascalTriangle {
             } 
             System.out.println(" ");
         }
+        for (int i = n-1; i >=1; i--) {
+            for (int j = 1; j <=i; j++)
+            {
+                if ((i%2==0 && j%2==0)||(i%2==1 && j%2==1)) {
+                    System.out.print("*");// PRINT
+                } else {
+                    System.out.print(" ");// SPACE
+                }   
+            } 
+            System.out.println(" ");
+        }
+       
     }    
     public static void main(String[] args) {
         System.out.print("Number of rows :- ");
