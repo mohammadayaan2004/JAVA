31217ec107f5ab7ed11b8abf546e37cb4f022613
Mohammad Ayaan Khan <mohammad.ayaan2004@gmail.com>
2025-01-11 20:05:51 +0530

    Your commit message

--- pattern/prog75_MirrorImageTrianglePattern.java
+++ pattern/prog75_MirrorImageTrianglePattern.java
@@ -3,12 +3,16 @@
  2 3 4
   3 4
    4
+  3 4
+ 2 3 4  
+1 2 3 4
 /*/
 import java.util.Scanner;
-public class prog71_ReverseNumberTrianglePattern 
+public class prog75_MirrorImageTrianglePattern 
 {
     public static void nForest(int n) 
     {
+        //upper pyramid
         for (int i = 1; i <=n; i++) 
         {
             for (int j = 1; j <=i-1; j++)
@@ -17,6 +21,21 @@ public class prog71_ReverseNumberTrianglePattern
             } 
             int k=i;
             for (int j = 1; j <=n-i+1; j++)
+            {
+                System.out.print(k+" ");// STAR
+                k++;
+            } 
+            System.out.println(" ");
+        } 
+        //lower pyramid
+        for (int i = 2; i <=n; i++) 
+        {
+            for (int j = 1; j <=n-i; j++)
+            {
+                System.out.print(" ");// SPACE    
+            } 
+            int k=n-i+1;
+            for (int j = 1; j <=i; j++)
             {
                 System.out.print(k+" ");// STAR
                 k++;
