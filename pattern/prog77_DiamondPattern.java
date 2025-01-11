190a9135645adc14e2a9dc0b778359416c390270
Mohammad Ayaan Khan <mohammad.ayaan2004@gmail.com>
2025-01-12 00:07:52 +0530

    Your commit message

---
+++ pattern/prog77_DiamondPattern.java
@@ -0,0 +1,42 @@
+/*/
+
+/*/
+import java.util.Scanner;
+public class prog77_DiamondPattern 
+{
+    public static void nForrest(int n)
+    {
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n-i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i=2;i<=n;i++)
+        {
+            for(int j=1;j<=i-1;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=n-i+1;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args)
+    {
+        System.out.print("Enter the number of rows: ");
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        nForrest(n);
+        sc.close();
+    }
+}
