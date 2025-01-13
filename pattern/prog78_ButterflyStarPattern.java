ac8e0d8cb488cf6562081a744d6fcbb61bbe2606
Mohammad Ayaan Khan <mohammad.ayaan2004@gmail.com>
2025-01-13 19:57:45 +0530

    Your commit message

--- pattern/prog78_ButterflyStarPattern.java
+++ pattern/prog78_ButterflyStarPattern.java
@@ -9,12 +9,11 @@ https://youtu.be/UTW2t4gOiJA?si=cUB1OmPj17SjLpeM
 *     *   
 /*/
 import java.util.Scanner;
-
 public class prog78_ButterflyStarPattern {
     public static void nForrest(int n) {
         for (int i = 1; i <= 2 * n - 1; i++) {
             for (int j = 1; j <= 2 * n - 1; j++) {
-                if ((j - i <= 0 && i + j <= 2 * n && j<=n)||(j + i >= 2*n &&  j-i >= 0 && j>=n)) {
+                if ((j <=i   && i + j <= 2 * n && j<=n)||(j + i >= 2*n &&  j >= i && j>=n))  {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
