8e9b7fa233f5bce6b893a0ae2c35b02cb48f3b0b
Mohammad Ayaan Khan <mohammad.ayaan2004@gmail.com>
2025-01-16 20:53:56 +0530

    Your commit message

--- pattern/prog86_HollowButterflyType2StarPattern.java
+++ pattern/prog86_HollowButterflyType2StarPattern.java
@@ -10,13 +10,9 @@
 import java.util.Scanner;
 public class prog86_HollowButterflyType2StarPattern {
     public static void nForrest(int n) {
-        for (int i = 1; i <= 2 * n-1; i++) {
+        for (int i = 1; i <= 2*n-1; i++) {
             for (int j = 1; j <= 2 * n; j++) {
-                
-                if(j == 2 * n || j == 1 ){
-                    System.out.print("*");
-                }
-                else if(i==j || i+j==2*n ){
+                if(j == 2 * n || j == 1 || (j<=n && ( i==j || i+j==2*n)) || (j>n && (i+j==2*n+1 || j-i==1))){
                     System.out.print("*");
                 }
                 else {
