500e79e12f36e15434a1f443c0deae40105f47a9
Mohammad Ayaan Khan <mohammad.ayaan2004@gmail.com>
2025-01-29 19:44:00 +0530

    Your Commit message

--- GFG/prog05_SumOFNumberINStrings.java
+++ GFG/prog05_SumOFNumberINStrings.java
@@ -1,6 +1,20 @@
-package GFG;
-import java.util.Scanner;
-public class prog05_SumOFNumberINStrings {
+/*
+https://www.geeksforgeeks.org/problems/sum-of-numbers-in-string-1587115621/1
+Sum of numbers in string
+Difficulty: BasicAccuracy: 63.01%Submissions: 60K+Points: 1
+Given a string str containing alphanumeric characters. The task is to calculate the sum of all the numbers present in the string.
+
+Examples:
+
+Input: s = "1abc23"
+Output: 24
+Explanation: 1 and 23 are numbers in the string which is added to get the sum as 24.
+Input: s = "geeks4geeks"
+Output: 4
+Explanation: 4 is the only number, so the sum is 4.
+Constraints:
+1 <= length of the string <= 105
+The sum of Numbers <= 105
     public static int findSum(String s) {
         int sum=0;
         String temp="0";
@@ -15,6 +29,26 @@ public class prog05_SumOFNumberINStrings {
             }
         }       
         return sum+Integer.parseInt(temp);
+    }
+        T.C=O(n^2)
+        S.C=O(n)
+ */
+package GFG;
+import java.util.Scanner;
+public class prog05_SumOfNumberInStrings {
+    public static int findSum(String s) {
+        int sum=0,num=0;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(Character.isDigit(ch)){
+                num=num*10+(ch -'0');
+            }
+            else{
+                sum+=num;
+                num=0;
+            }
+        }       
+        return sum+num;
     }
 
     public static void main(String[] args) {
