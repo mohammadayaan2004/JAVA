7135f6f84fb412e875bd085c613938138f4d05b8
Mohammad Ayaan Khan <mohammad.ayaan2004@gmail.com>
2025-01-30 16:32:29 +0530

    Your Commit message

--- GFG/prog03_NarcissisticNumber.java
+++ GFG/prog03_NarcissisticNumber.java
@@ -1,17 +1,5 @@
 /*
-https://www.geeksforgeeks.org/problems/sum-of-digits1742/1
-Given a number n. Find the sum of all the digits of n.
 
-Examples:
-
-Input: n = 12
-Output: 3
-Explanation: Sum of 12's digits: 1 + 2 = 3
-Input: n = 23
-Output 5
-Explanation: Sum of 23's digits: 2 + 3 = 5
-Constraints:
-1<= n <=105
 
 
  */
