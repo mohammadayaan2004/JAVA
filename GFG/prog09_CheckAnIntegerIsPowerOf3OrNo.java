da4b8a1a93281e3ed8027bfa71e21f04a9bbda65
Mohammad Ayaan Khan <mohammad.ayaan2004@gmail.com>
2025-02-03 22:32:07 +0530

    GFG prog09 Check An Integer Is Power Of 3 Or No

--- GFG/prog09_CheckAnIntegerIsPowerOf3OrNo.java
+++ GFG/prog09_CheckAnIntegerIsPowerOf3OrNo.java
@@ -1,4 +1,39 @@
 /*
+https://www.geeksforgeeks.org/problems/check-a-integer-is-power-of-3-or-not3850/1
+
+Check an Integer is power of 3 or not
+Given a positive integer N, write a function to find if it is a power of three or not.
+
+Example 1:
+
+Input:
+N = 3
+Output:
+Yes
+Explanation:
+31 is a power of 3.
+ 
+
+Example 2:
+
+Input:
+N = 5
+Output:
+No
+Explanation:
+5 is not a power of 3.
+ 
+
+Your Task:
+
+You don't need to read input or print anything. Your task is to complete the function isPowerof3() which takes an integer N and returns "Yes" if N is a power of 3, else "No" if N is not a power of 3.
+
+Expected Time Complexity: O(1)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1<=N<=(32-bit largest integer)
+
 */
 package GFG;
 import java.util.Scanner;
