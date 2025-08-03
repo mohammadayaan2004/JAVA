fedb9000aad2158152359429aaeab7283a76a961
Mohammad Ayaan Khan <mohammad.ayaan2004@gmail.com>
2025-08-03 18:16:48 +0530

    program 57 Equal Function

---
+++ basic/prog57_EqualFunction.java
@@ -0,0 +1,14 @@
+//.equals() is a method used to compare two objects to check if their contents (or values) are the same.
+import java.util.Scanner;
+public class prog57_EqualFunction {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter :- ");
+        String name1=sc.nextLine();
+        System.out.print("Enter :- ");        
+        String name2=sc.nextLine();
+        System.out.println(name1.equals(name2));
+        System.out.println(name1.equals("sgbhsx"));
+        sc.close();
+    }    
+}
