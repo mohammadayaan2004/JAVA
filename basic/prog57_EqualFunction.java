558639faf51979a43a2fd11722d1b106c5c031b3
Mohammad Ayaan Khan <mohammad.ayaan2004@gmail.com>
2025-08-03 23:21:19 +0530

    program 57 Equal Function

--- basic/prog57_EqualFunction.java
+++ basic/prog57_EqualFunction.java
@@ -8,7 +8,7 @@ public class prog57_EqualFunction {
         System.out.print("Enter :- ");        
         String name2=sc.nextLine();
         System.out.println(name1.equals(name2));
-        System.out.println(name1.equals("sgbhsx"));
+        System.out.println(name1.equals("MAK"));
         sc.close();
     }    
 }
