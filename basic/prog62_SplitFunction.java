de90391a5b6755c87f83e6eabe63ab6d1ace370e
Mohammad Ayaan Khan <mohammad.ayaan2004@gmail.com>
2025-08-10 18:20:55 +0530

    program 61 Split Function

--- basic/prog62_SplitFunction.java
+++ basic/prog62_SplitFunction.java
@@ -1,3 +1,4 @@
+//It is used to split a string into an array of substrings based on a regular expression (regex) 
 import java.util.Arrays;
 import java.util.Scanner;
 public class prog62_SplitFunction {
@@ -7,7 +8,6 @@ public class prog62_SplitFunction {
         String name=sc.nextLine();      
         String arrname[]=name.split(":");
         System.out.println(Arrays.toString(arrname));
-
         sc.close();    
      }
 }
