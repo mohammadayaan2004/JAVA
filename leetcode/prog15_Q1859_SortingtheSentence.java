d5bd9060270d8adcef48b6c362876a0510204aa5
mohammadayaan2004 <mohammad.ayaan2004@gmail.com>
2026-01-08 01:35:54 +0530

    Leetcode Program 15 Q1859 Sorting The Sentence

---
+++ leetcode/prog15_Q1859_SortingtheSentence.java
@@ -0,0 +1,57 @@
+/* 
+
+https://leetcode.com/problems/sorting-the-sentence/
+
+A sentence is a list of words that are separated by a single space with no leading or trailing spaces. Each word consists of lowercase and uppercase English letters.
+A sentence can be shuffled by appending the 1-indexed word position to each word then rearranging the words in the sentence.
+For example, the sentence "This is a sentence" can be shuffled as "sentence4 a3 is2 This1" or "is2 sentence4 This1 a3".
+Given a shuffled sentence s containing no more than 9 words, reconstruct and return the original sentence.
+
+ 
+
+Example 1:
+
+Input: s = "is2 sentence4 This1 a3"
+Output: "This is a sentence"
+Explanation: Sort the words in s to their original positions "This1 is2 a3 sentence4", then remove the numbers.
+Example 2:
+
+Input: s = "Myself2 Me1 I4 and3"
+Output: "Me Myself and I"
+Explanation: Sort the words in s to their original positions "Me1 Myself2 and3 I4", then remove the numbers.
+ 
+
+Constraints:
+
+2 <= s.length <= 200
+s consists of lowercase and uppercase English letters, spaces, and digits from 1 to 9.
+The number of words in s is between 1 and 9.
+The words in s are separated by a single space.
+s contains no leading or trailing spaces.
+*/
+package leetcode;
+
+import java.util.Scanner;
+import java.util.TreeMap;
+import java.util.Map;
+public class prog15_Q1859_SortingtheSentence {
+    public static String sortSentence(String s) {
+        String[] arr = s.split(" ");;
+        Map<Integer , String > map = new TreeMap<>();
+        for (String str : arr) {
+            String digits = str.replaceAll("[^0-9]", "");
+            String stt=str.replaceAll("[0-9]", "");
+            int digit = Integer.parseInt(digits);
+            map.put(digit,stt);
+        }
+        return String.join(" ", map.values());
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please Enter a String :- ");
+        String num = sc.nextLine();
+
+        System.out.print("Result :- " + sortSentence(num));
+        sc.close();
+    }
+}
