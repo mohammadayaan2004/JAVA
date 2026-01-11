9a0c6a641781c298acd667f511e848b9073d7bb7
mohammadayaan2004 <mohammad.ayaan2004@gmail.com>
2026-01-11 20:47:46 +0530

    Leetcode Program 25 Q 477 Total Hamming Distance

---
+++ leetcode/prog25_Q477_TotalHammingDistance.java
@@ -0,0 +1,65 @@
+/* 
+https://leetcode.com/problems/total-hamming-distance/
+
+The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+
+Given an integer array nums, return the sum of Hamming distances between all the pairs of the integers in nums.
+
+ 
+
+Example 1:
+
+Input: nums = [4,14,2]
+Output: 6
+Explanation: In binary representation, the 4 is 0100, 14 is 1110, and 2 is 0010 (just
+showing the four bits relevant in this case).
+The answer will be:
+HammingDistance(4, 14) + HammingDistance(4, 2) + HammingDistance(14, 2) = 2 + 2 + 2 = 6.
+Example 2:
+
+Input: nums = [4,14,4]
+Output: 4
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+0 <= nums[i] <= 109
+The answer for the given input will fit in a 32-bit integer
+*/
+package leetcode;
+
+import java.util.Scanner;
+
+public class prog25_Q477_TotalHammingDistance {
+    public static int totalHammingDistance(int[] nums) {
+        int sum = 0;
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                sum+=Integer.bitCount(nums[i]^nums[j]);
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // input size
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        // input array
+        int[] nums = new int[n];
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.println("Result: " + totalHammingDistance(nums));
+
+        sc.close();
+    }
+}
